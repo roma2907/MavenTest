ffda9bbad7a8f09c420fd296a62c9f4934acdd83
Ramage <=>
2015-09-11 16:17:59 +0200

    first commit

---
+++ src/main/java/fr/istic/master2/App.java
@@ -0,0 +1,26 @@
+package fr.istic.master2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	/**
+	 * Fonction principale du programme.
+	 * @param args : arguments du programme
+	 */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        if(true){
+        	System.err.println(true);
+        }else{
+        	System.err.println(false);
+        }
+    }
+    
+    public int codeMort(){
+    	return 1;
+    }
+}
